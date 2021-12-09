af34d47311088387f15051cdec2ca91686c3e76d
0vertex <89994776+0vertex@users.noreply.github.com>
2021-12-09 21:30:00 +0530

    Added custom banner layout with partial fixes

--- MovieLog/Common/Layouts/CollectionViewBannerLayout.swift
+++ MovieLog/Common/Layouts/CollectionViewBannerLayout.swift
@@ -8,20 +8,88 @@
 import CoreUIKit
 import UIKit
 
-class CollectionViewBannerLayout: BaseCollectionViewFlowLayout {
+class CollectionViewBannerLayout: UICollectionViewLayout {
+    
+    private var cellWidth: CGFloat = .zero
+    private var cellHeight: CGFloat = .zero
+    private let horizontalMargin: CGFloat = 30
+    private var numberOfItemsInFirstSection = 0
+    private var cachedCollectionViewContentSize: CGSize = .zero
+    private var cachedCollectionViewLayoutAttributes = [UICollectionViewLayoutAttributes]()
     
     override func prepare() {
         super.prepare()
         
         guard let collectionView = self.collectionView else { return }
-        self.setupCollectionViewConfigs()
+
+        self.cellWidth = collectionView.bounds.width - horizontalMargin
+        self.cellHeight = collectionView.bounds.height
+        self.numberOfItemsInFirstSection = collectionView.numberOfItems(inSection: 0)
+        self.cachedCollectionViewContentSize = CGSize(width: (self.cellWidth * CGFloat(self.numberOfItemsInFirstSection)), height: self.cellHeight)
         
-        // Setup cell
-        let availableWidth = collectionView.bounds.width
-        let availableHeight = collectionView.bounds.height
-        let cellWidth = availableWidth - 50
+        // Generate attributes for cells
+        for currentIndex in (0..<self.numberOfItemsInFirstSection) {
+            let attr = UICollectionViewLayoutAttributes(forCellWith: IndexPath(row: currentIndex, section: 0))
+            attr.frame = CGRect(x: CGFloat(currentIndex) * self.cellWidth, y: 0.0,
+                                width: self.cellWidth, height: self.cellHeight)
+            self.cachedCollectionViewLayoutAttributes.append(attr)
+        }
         
-        self.itemSize = CGSize(width: cellWidth, height: availableHeight)
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return cachedCollectionViewContentSize
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        guard let collectionView = collectionView else { return false }
+        return (newBounds.size.equalTo(collectionView.bounds.size) == false)
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        return self.cachedCollectionViewLayoutAttributes[indexPath.row]
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var matchedAttributes = [UICollectionViewLayoutAttributes]()
+        
+        guard let lastIndex = self.cachedCollectionViewLayoutAttributes.indices.last,
+              let firstMatchedIndex = self.binarySearch(rect, fromIndex: 0, toIndex: lastIndex) else { return matchedAttributes }
+        
+        // Check for matching attributes from end
+        for attribute in self.cachedCollectionViewLayoutAttributes[..<firstMatchedIndex].reversed() {
+            guard attribute.frame.maxY >= rect.minY else { break }
+            matchedAttributes.append(attribute)
+        }
+        
+        // Check for matching attributes from start
+        for attribute in self.cachedCollectionViewLayoutAttributes[firstMatchedIndex...] {
+            guard attribute.frame.minY <= rect.maxY else { break }
+            matchedAttributes.append(attribute)
+        }
+        
+        return matchedAttributes
+    }
+    
+}
+
+extension CollectionViewBannerLayout {
+    
+    func binarySearch(_ rect: CGRect, fromIndex startIndex: Int, toIndex endIndex: Int) -> Int? {
+        if endIndex < startIndex { return nil }
+
+        let midIndex = (startIndex + endIndex) / 2
+        let midAttribute = self.cachedCollectionViewLayoutAttributes[midIndex]
+        
+        if midAttribute.frame.intersects(rect) {
+            return midIndex
+        } else {
+            if midAttribute.frame.maxY < rect.minY {
+                return self.binarySearch(rect, fromIndex: (midIndex + 1), toIndex: endIndex)
+            } else {
+                return self.binarySearch(rect, fromIndex: startIndex, toIndex: (midIndex - 1))
+            }
+        }
     }
     
 }
